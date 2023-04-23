94fc0d33046208b1d0fb05a34150c7378ca1c7ba
Bea <99785877+Bea290702@users.noreply.github.com>
2023-04-23 21:01:05 +0300

    version 1.0

--- src/main/java/com/example/pong/Controller.java
+++ src/main/java/com/example/pong/Controller.java
@@ -9,24 +9,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
-
-import static javafx.scene.input.KeyCode.*;
 
 public class Controller {
 
@@ -34,16 +29,20 @@ public class Controller {
         NONE, LEFT, RIGHT, A, D
     }
 
+    private static int score = 0;
+    private static int score1 = 0;
+    private static int score2 = 0;
+    private static Text scoreText = new Text();
+    private static Text scoreMulti1 = new Text();
+    private static Text scoreMulti2 = new Text();
+
     private static Scene scene;
     private static Scene playScene;
-    private static Scene restartScene;
     private static Scene sceneGameOver;
     private static Scene multiplayerScene;
 
     private static DropShadow dropShadow;
 
-    @FXML
-    private static AnchorPane pane = new AnchorPane();
     @FXML
     private static Pane root = new Pane();
     @FXML
@@ -51,7 +50,7 @@ public class Controller {
 
     private static Stage playStage = new Stage();
     @FXML
-    private static Stage gameStage = new Stage();
+    private static Stage singlePlayerStage = new Stage();
     private static Stage stageGameOver = new Stage();
     private static Stage restartStage = new Stage();
     private static Stage multiplayerStage = new Stage();
@@ -66,8 +65,9 @@ public class Controller {
     @FXML
     private static Button restartButton = new Button();
     @FXML
-    private static Button multiplayerButton = new Button();
-    private static Parent parent;
+    private static Button backToStartButtonSinglePlayer = new Button();
+    @FXML
+    private static Button backToStartButtonMultiPlayer = new Button();
 
     private static final int APP_W = 500;
     private static final int APP_H = 700;
@@ -98,8 +98,7 @@ public class Controller {
     private static UserAction action = UserAction.NONE;
 
 
-    @FXML
-    public Stage play() throws IOException {
+    public static Stage play() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Pong_Game.class.getResource("start-the-game.fxml"));
         playScene = new Scene(fxmlLoader.load(),500,450);
 
@@ -113,17 +112,7 @@ public class Controller {
     }
 
     public static Parent createContent() throws IOException {
-        playStage.close();
-
-        restartButton.setPrefHeight(117);
-        restartButton.setPrefWidth(113);
-        restartButton.setText("");
-
-        restartButton.setLayoutX(192);
-        restartButton.setLayoutY(270);
-
-        restartButton.setStyle("-fx-border-color: '#ff1ea9'; -fx-stroke-width: 4; -fx-stroke: '#ff3535'; -fx-border-width: 5; -fx-background-color: transparent;-fx-shape: \"M58.828,16.208l-3.686,4.735c7.944,6.182,11.908,16.191,10.345,26.123C63.121,62.112,48.954,72.432,33.908,70.06C18.863,67.69,8.547,53.522,10.912,38.477c1.146-7.289,5.063-13.694,11.028-18.037c5.207-3.79,11.433-5.613,17.776-5.252l-5.187,5.442l3.848,3.671l8.188-8.596l0.002,0.003l3.668-3.852L46.39,8.188l-0.002,0.001L37.795,0l-3.671,3.852l5.6,5.334c-7.613-0.36-15.065,1.853-21.316,6.403c-7.26,5.286-12.027,13.083-13.423,21.956c-2.879,18.313,9.676,35.558,27.989,38.442c1.763,0.277,3.514,0.411,5.245,0.411c16.254-0.001,30.591-11.85,33.195-28.4C73.317,35.911,68.494,23.73,58.828,16.208z\"");
-
+        helper();
         restartButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -134,6 +123,34 @@ public class Controller {
                 }
             }
         });
+
+        root.getChildren().addAll(scoreText, ball, rectangle, restartButton);//scoreLabel, backToStartButtonSinlgePlayer - add if you want to exit to main window
+        return root;
+    }
+
+    private static void helper() {
+        /*scoreLabel.setText("SCORE : " + score);
+        scoreLabel.setTextFill(Color.BLACK);
+        scoreLabel.setTranslateX(APP_W - 90);
+        scoreLabel.setTranslateY(10);
+        scoreLabel.setFont(new Font(15));*/
+        scoreText.setX(APP_W - 70);
+        scoreText.setY(20);
+
+        restartButton.setPrefHeight(117);
+        restartButton.setPrefWidth(113);
+        backToStartButtonSinglePlayer.setPrefHeight(32);
+        backToStartButtonSinglePlayer.setPrefWidth(35);
+        restartButton.setText("");
+        backToStartButtonSinglePlayer.setText("");
+
+        restartButton.setLayoutX(192);
+        restartButton.setLayoutY(270);
+        backToStartButtonSinglePlayer.setLayoutX(20);
+        backToStartButtonSinglePlayer.setLayoutY(20);
+
+        restartButton.setStyle("-fx-border-color: '#ff1ea9'; -fx-stroke-width: 4; -fx-stroke: '#ff3535'; -fx-border-width: 5; -fx-background-color: transparent;-fx-shape: \"M58.828,16.208l-3.686,4.735c7.944,6.182,11.908,16.191,10.345,26.123C63.121,62.112,48.954,72.432,33.908,70.06C18.863,67.69,8.547,53.522,10.912,38.477c1.146-7.289,5.063-13.694,11.028-18.037c5.207-3.79,11.433-5.613,17.776-5.252l-5.187,5.442l3.848,3.671l8.188-8.596l0.002,0.003l3.668-3.852L46.39,8.188l-0.002,0.001L37.795,0l-3.671,3.852l5.6,5.334c-7.613-0.36-15.065,1.853-21.316,6.403c-7.26,5.286-12.027,13.083-13.423,21.956c-2.879,18.313,9.676,35.558,27.989,38.442c1.763,0.277,3.514,0.411,5.245,0.411c16.254-0.001,30.591-11.85,33.195-28.4C73.317,35.911,68.494,23.73,58.828,16.208z\"");
+        backToStartButtonSinglePlayer.setStyle("-fx-border-color: '#ff1ea9'; -fx-stroke-width: 4; -fx-stroke: '#ff3535'; -fx-border-width: 4; -fx-background-color: transparent; -fx-shape: \"M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z\"; -fx-stroke: '#000'; -fx-stroke-line-cap: round; -fx-stroke-line-join: round; -fx-stroke-width: 48mp");
 
         root.setPrefHeight(APP_H);
         root.setPrefWidth(APP_W);
@@ -155,8 +172,9 @@ public class Controller {
 
         restartButton.setDisable(true);
         restartButton.setOpacity(0);
-        root.getChildren().addAll(ball, rectangle, restartButton);
-        return root;
+
+        backToStartButtonSinglePlayer.setDisable(true);
+        backToStartButtonSinglePlayer.setOpacity(0);
     }
 
     public static void collisionCheck() throws IOException {
@@ -200,6 +218,8 @@ public class Controller {
             rectangle.setEffect(new BoxBlur(10,10,3));
 
             restartButton.setOpacity(1);
+            backToStartButtonSinglePlayer.setOpacity(1);
+            backToStartButtonSinglePlayer.setDisable(false);
         }
 
         //if ball meets the top of the window
@@ -207,11 +227,12 @@ public class Controller {
             ball.setY(-ball.yV);
             playBallSound(ballSound,ballM);
         }
-
         //if ball meets the rectangle
         else if (ball.getTranslateY() + BALL_RADIUS >= APP_H - RECT_H
                 && ball.getTranslateX() + BALL_RADIUS >= rectangle.getTranslateX()
                 && ball.getTranslateX() - BALL_RADIUS <= rectangle.getTranslateX() + RECT_W) {
+            ++score;
+
             ball.setY(Math.abs(ball.yV));
 
             playBallSound(ballSound,ballM);
@@ -229,6 +250,8 @@ public class Controller {
             ball.setX(ball.xV);
             ball.setY(-ball.yV);
         }
+
+        scoreText.setText("SCORE : " + score);
     }
 
     @FXML
@@ -263,18 +286,22 @@ public class Controller {
             }
         });
 
-        gameStage.setTitle("Ball Game");
-        gameStage.setOpacity(1);
-        gameStage.setScene(scene);
-        gameStage.setResizable(false);
-        gameStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/pong/style/game_icon.jpg")));
-        gameStage.show();
+        singlePlayerStage.setTitle("Ball Game");
+        singlePlayerStage.setOpacity(1);
+        singlePlayerStage.setScene(scene);
+        singlePlayerStage.setResizable(false);
+        singlePlayerStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/pong/style/game_icon.jpg")));
+        singlePlayerStage.show();
     }
 
     @FXML
     private static void restartGame() throws IOException {
+        score = 0;
+
         restartButton.setDisable(true);
         restartButton.setOpacity(0);
+        backToStartButtonSinglePlayer.setDisable(true);
+        backToStartButtonSinglePlayer.setOpacity(0);
 
         //set the elements to base position
         rectangle.setTranslateX(APP_W / 2.5);
@@ -291,28 +318,17 @@ public class Controller {
         ball.setTranslateX(APP_W / 2);
         ball.setTranslateY(APP_H / 2);
 
-        gameStage.setOpacity(1);
-    }
-
-    private static void stopGame() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Pong_Game.class.getResource("restartWindow.fxml"));
-        restartScene = new Scene(fxmlLoader.load(),500,700);
-
-        restartStage.setScene(restartScene);
-        restartStage.setOpacity(0.8);
-        restartButton.setOpacity(1);
-
-        restartStage.show();
-        restartStage.close();
+        singlePlayerStage.setOpacity(1);
     }
 
     @FXML
     private void closeGame() {
-        gameStage.close();
+        singlePlayerStage.close();
         restartStage.close();
         stageGameOver.close();
     }
 
+    //opens a different window for game over
     private static void openGameOver() throws IOException {
         FXMLLoader fx = new FXMLLoader(Pong_Game.class.getResource("game_over.fxml"));
         sceneGameOver = new Scene(fx.load(),400,200);
@@ -365,13 +381,15 @@ public class Controller {
             public void handle(ActionEvent event) {
                 try {
                    restartMultiplayer();
+                   restartButton.setOpacity(0);
+                   restartButton.setDisable(true);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             }
         });
 
-        multiplayerPane.getChildren().addAll(player1Ball,player2Ball,player1Stick,player2Stick,separator,restartButton);
+        multiplayerPane.getChildren().addAll(scoreMulti1,scoreMulti2,player1Ball,player2Ball,player1Stick,player2Stick,separator,restartButton);//backToStartButtonMultiPlayer - add it if you want to exit to main window
         return multiplayerPane;
     }
 
@@ -420,6 +438,11 @@ public class Controller {
     }
 
     private static void initialize() {
+        scoreMulti1.setX(10);
+        scoreMulti1.setY(20);
+        scoreMulti2.setX(MULTI_W - 70);
+        scoreMulti2.setY(20);
+
         restartButton.setPrefHeight(117);
         restartButton.setPrefWidth(113);
         restartButton.setText("");
@@ -427,7 +450,14 @@ public class Controller {
         restartButton.setLayoutX(343);
         restartButton.setLayoutY(279);
 
+        backToStartButtonMultiPlayer.setPrefHeight(32);
+        backToStartButtonMultiPlayer.setPrefWidth(32);
+
+        backToStartButtonMultiPlayer.setLayoutX(20);
+        backToStartButtonMultiPlayer.setLayoutY(20);
+
         restartButton.setStyle("-fx-border-color: '#ff1ea9'; -fx-stroke-width: 4; -fx-stroke: '#ff3535'; -fx-border-width: 5; -fx-background-color: transparent;-fx-shape: \"M58.828,16.208l-3.686,4.735c7.944,6.182,11.908,16.191,10.345,26.123C63.121,62.112,48.954,72.432,33.908,70.06C18.863,67.69,8.547,53.522,10.912,38.477c1.146-7.289,5.063-13.694,11.028-18.037c5.207-3.79,11.433-5.613,17.776-5.252l-5.187,5.442l3.848,3.671l8.188-8.596l0.002,0.003l3.668-3.852L46.39,8.188l-0.002,0.001L37.795,0l-3.671,3.852l5.6,5.334c-7.613-0.36-15.065,1.853-21.316,6.403c-7.26,5.286-12.027,13.083-13.423,21.956c-2.879,18.313,9.676,35.558,27.989,38.442c1.763,0.277,3.514,0.411,5.245,0.411c16.254-0.001,30.591-11.85,33.195-28.4C73.317,35.911,68.494,23.73,58.828,16.208z\"");
+        backToStartButtonMultiPlayer.setStyle("-fx-border-color: '#ff1ea9'; -fx-stroke-width: 4; -fx-stroke: '#ff3535'; -fx-border-width: 4; -fx-background-color: transparent; -fx-shape: \"M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z\"; -fx-stroke: '#000'; -fx-stroke-line-cap: round; -fx-stroke-line-join: round; -fx-stroke-width: 48mp");
 
         player1Ball = new Ball(BALL_RADIUS,MULTI_W / 4,MULTI_H / 2 + MULTI_H / 4);
         player2Ball = new Ball(BALL_RADIUS,(MULTI_W/2) + MULTI_W / 4,MULTI_H / 2 + MULTI_H / 6);
@@ -465,6 +495,8 @@ public class Controller {
         restartButton.setEffect(dropShadow);
         restartButton.setDisable(true);
         restartButton.setOpacity(0);
+        backToStartButtonMultiPlayer.setDisable(true);
+        backToStartButtonMultiPlayer.setOpacity(0);
 
         multiplayerPane.setPrefHeight(MULTI_H);
         multiplayerPane.setPrefWidth(MULTI_W);
@@ -540,6 +572,7 @@ public class Controller {
             player2Ball.setY(0);
 
             restartButton.setDisable(false);
+            backToStartButtonMultiPlayer.setDisable(false);
 
             player1Ball.setEffect(new BoxBlur(10,10,3));
             player1Stick.setEffect(new BoxBlur(10,10,3));
@@ -550,6 +583,7 @@ public class Controller {
             restartButton.setEffect(dropShadow);
 
             restartButton.setOpacity(1);
+            backToStartButtonMultiPlayer.setOpacity(1);
         }
     }
 
@@ -569,11 +603,25 @@ public class Controller {
             player1Ball.setY(-player1Ball.yV);
             playBallSound(ballSound,ballM);
         }
+        //if ball meets the bottom of the window
+        else if (player1Ball.getTranslateY() >= MULTI_H - BALL_RADIUS) {
+            player1Ball.setX(0);
+            player1Ball.setY(0);
+            restartButton.setDisable(false);
+            restartButton.setEffect(dropShadow);
+
+            player1Ball.setEffect(new BoxBlur(10,10,3));
+
+            restartButton.setOpacity(1);
+            backToStartButtonSinglePlayer.setOpacity(1);
+            backToStartButtonSinglePlayer.setDisable(false);
+        }
         //if ball meets the rectangle
         else if (player1Ball.getTranslateY() + BALL_RADIUS >= MULTI_H - RECT_H
                 && player1Ball.getTranslateX() + BALL_RADIUS >= player1Stick.getTranslateX()
                 && player1Ball.getTranslateX() - BALL_RADIUS <= player1Stick.getTranslateX() + RECT_W) {
             player1Ball.setY(Math.abs(player1Ball.yV));
+            ++score1;
 
             playBallSound(ballSound,ballM);
 
@@ -590,62 +638,70 @@ public class Controller {
             player1Ball.setX(player1Ball.xV);
             player1Ball.setY(-player1Ball.yV);
         }
+        scoreMulti1.setText("SCORE : " + score1);
     }
 
     private static void collisionBall2() {
         //stops when balls hit the left side
         if (player2Ball.getTranslateX() - BALL_RADIUS <= MULTI_W / 2) {
             player2Ball.setX(Math.abs(player2Ball.xV));
-            playBallSound(ballSound,ballM);
+            playBallSound(ballSound, ballM);
         }
         //stops when balls hit the right side
         else if (player2Ball.getTranslateX() + BALL_RADIUS >= MULTI_W) {
             player2Ball.setX(-player2Ball.xV);
-            playBallSound(ballSound,ballM);
+            playBallSound(ballSound, ballM);
         }
         //if ball meets the top of the window
-        else if (player2Ball.getTranslateY() - BALL_RADIUS <= 0){
+        else if (player2Ball.getTranslateY() - BALL_RADIUS <= 0) {
             player2Ball.setY(-player2Ball.yV);
-            playBallSound(ballSound,ballM);
+            playBallSound(ballSound, ballM);
+        }
+        //if ball meets the bottom of the window
+        else if (player2Ball.getTranslateY() >= MULTI_H - BALL_RADIUS) {
+            player2Ball.setX(0);
+            player2Ball.setY(0);
+            restartButton.setDisable(false);
+            restartButton.setEffect(dropShadow);
+
+            player2Ball.setEffect(new BoxBlur(10, 10, 3));
+
+            restartButton.setOpacity(1);
+            backToStartButtonSinglePlayer.setOpacity(1);
+            backToStartButtonSinglePlayer.setDisable(false);
         }
         //if ball meets the rectangle
         else if (player2Ball.getTranslateY() + BALL_RADIUS >= APP_H - RECT_H
                 && player2Ball.getTranslateX() + BALL_RADIUS >= player2Stick.getTranslateX()
                 && player2Ball.getTranslateX() - BALL_RADIUS <= player2Stick.getTranslateX() + RECT_W) {
             player2Ball.setY(Math.abs(player2Ball.yV));
+            ++score2;
 
-            playBallSound(ballSound,ballM);
+            playBallSound(ballSound, ballM);
 
             //optional for more difficulty
             player2Ball.yV++;
             if (player2Ball.xV > 0) {
                 //optional for more difficulty
                 ++player2Ball.xV;
-            }
-            else {
+            } else {
                 --player2Ball.xV;
             }
 
             player2Ball.setX(player2Ball.xV);
             player2Ball.setY(-player2Ball.yV);
         }
-    }
-
-    public class Key extends KeyAdapter {
-        public void keyPressed(KeyEvent e) {
-            player1Stick.keyPressed(e);
-            player2Stick.keyPressed(e);
-        }
-
-        public void keyReleased(KeyEvent e) {
-            player1Stick.keyReleased(e);
-            player2Stick.keyReleased(e);
-        }
+        scoreMulti2.setText("SCORE : " + score2);
     }
 
     private static void restartMultiplayer() throws IOException {
+        score1 = 0;
+        score2 = 0;
+
         restartButton.setDisable(true);
         restartButton.setOpacity(0);
+        backToStartButtonMultiPlayer.setDisable(true);
+        backToStartButtonMultiPlayer.setOpacity(0);
 
         //set the elements to base position
         player1Stick.setTranslateX(MULTI_W / 4);
@@ -673,6 +729,27 @@ public class Controller {
         player2Ball.setY(2);
 
         multiplayerStage.setOpacity(1);
+    }
+
+
+    ///BACK TO THE MAIN PAGE
+
+    private static void backToStartWhenButtonPressedSinglePlayer() throws IOException {
+        restartButton.setDisable(true);
+        restartButton.setOpacity(0);
+        backToStartButtonSinglePlayer.setOpacity(0);
+        backToStartButtonSinglePlayer.setDisable(true);
+
+        play();
+    }
+
+    private static void backToStartWhenButtonPressedMultiPlayer() throws IOException {
+        restartButton.setDisable(true);
+        restartButton.setOpacity(0);
+        backToStartButtonMultiPlayer.setOpacity(0);
+        backToStartButtonMultiPlayer.setDisable(true);
+
+        play();
     }
 }
 
